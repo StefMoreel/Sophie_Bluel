d16e0422699845cbfaa4c53390600fa5473c5274
Stephane Moreel <stef.ksp@gmail.com>
2025-05-31 13:00:29 +0200

    Récupération des travaux depuis le back end

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,17 +1,31 @@
-console.log("test");
+// Récupération et stockage des travaux depuis l API au format json
+async function generesWorks (){
+    const reponse = await fetch("http://localhost:5678/api/works");
+    const works = await reponse.json();
 
-fetch("http://localhost:5678/api/works");
+    //Stockage de la réponse dans un tableau travaux
+   
 
+    for (let i=0 ; i<works.length;i++){
+        const work = works[i];
+        addWork(work);
+    }
+}
+function addWork (work){
 // Création des éléments dans le DOM
-const figureElement = document.createElement("figure");
+    const figureElement = document.createElement("figure");
 
-const imgElement = document.createElement("img");
+    const imgElement = document.createElement("img");
+    imgElement.src = work.imageUrl;
 
+    const figcaptionElement = document.createElement("figcaption");
+    figcaptionElement.innerText = work.title;
 
-const figcaptionElement = document.createElement("figcaption");
+    // Rattacher éléments créés au parent
+    const gallery = document.querySelector(".gallery");
 
-// Rattacher éléments créés au parent
-const gallery = document.querySelector(".gallery");
-gallery.appendChild(figureElement);
-figureElement.appendChild(imgElement);
-figureElement.appendChild(figcaptionElement);
+    gallery.appendChild(figureElement);
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(figcaptionElement);
+}
+generesWorks()
