60cee7c72886307f4bf2a442a16348a710e10976
Stephane Moreel <stef.ksp@gmail.com>
2025-06-03 17:27:37 +0200

    Filtres dynamiques par catégorie + CSS homepage

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,82 +1,165 @@
-// Récupération et stockage des travaux depuis l API au format json
-
-    //Création de la fonction de récupération des travaux depuis l'API
-async function getWorks (){
-    //Stockage de la fonction fetch dans la variable reponse
+//Fonction callApi
+    //Requete de la route works de l'API (GET)
     const reponse = await fetch("http://localhost:5678/api/works");
-    //Stockage de la réponse (= travaux) dans la variable works
+    //Réponse de la requete au format JSON
     const works = await reponse.json();
-    //Boucle sur les travaux
-    for (let i=0 ; i<works.length; i++){
-    //Stockage de chaque itération [i] dans la variable work
-    const work = works[i];
-    //Appel de la fonction d'affichage de chaque élément sur le site    
-addWork(work);
-    }
-//Filtrer les works selon la catégorie au click
-    //Ajout d'un listnener sur le btn-filters : click + fonction filtrer par catégorie
+
+//Fonction genererWorks
+    // Parcours des objet de la route works de l'APÏ
+    for (let i=0; i < works.length ; i++) {
+    // Création des éléments dans le DOM
+        // Création de la balise figure
+        const figureElement = document.createElement("figure");
+        // Création de la balise img (enfant de figure)
+        const imgElement = document.createElement("img");
+        // Affichage et identification de l'élément dans la baslise img depuis la source dans la route de l'API
+        imgElement.src = works[i].imageUrl;
+        // Création de la balise figcation (enfant de figure)
+        const figcaptionElement = document.createElement("figcaption");
+        // Affichage et identification de l'élément dans le texte de la balise figcaption depuis la source dans la route de l'API
+        figcaptionElement.innerText = works[i].title;
+    // Rattacher éléments créés au parent
+        //Ciblage de l'élémnent parent
+        const gallery = document.querySelector(".gallery");
+        //Insertion de la balise figure => enfant de gallery
+        gallery.appendChild(figureElement);
+        //Insertion de la balise img + affichage élément => enfant de figure
+        figureElement.appendChild(imgElement);
+        //Insertion de la balise figcation + affichage élément => enfant de figure
+        figureElement.appendChild(figcaptionElement);
+
+
+// Fonction catFilter
+    //Ajout d'un listener sur le bouton "Tous"
     const boutonsFiltersTous = document.querySelector(".all");
-    boutonsFiltersTous.addEventListener("click", function() {
-    //Fonction filter par id de la catégorie pour toutes les catégories
-        const categoriesFilter = works.filter(function (work){
-        return work.category.id;
-        });
-     console.log(categoriesFilter);
-    //  document.querySelector(".gallery").innerHTML = "";
-    //  addWork(categoriesFilter);
+    boutonsFiltersTous.addEventListener("click", function(){
+        //Effacement de la balise gallery 
+        document.querySelector(".gallery").innerHTML = "";
+        //Parcours des objet de la route works de l'APÏ 
+        for (let i=0; i < works.length ; i++) {
+        // Création des éléments dans le DOM
+            // Création de la balise figure
+            const figureElement = document.createElement("figure");
+            // Création de la balise img (enfant de figure)
+            const imgElement = document.createElement("img");
+            // Affichage et identification de l'élément dans la baslise img depuis la source dans la route de l'API
+            imgElement.src = works[i].imageUrl;
+            // Création de la balise figcation (enfant de figure)
+            const figcaptionElement = document.createElement("figcaption");
+            // Affichage et identification de l'élément dans le texte de la balise figcaption depuis la source dans la route de l'API
+            figcaptionElement.innerText = works[i].title;
+        // Rattacher éléments créés au parent
+            //Ciblage de l'élémnent parent
+            const gallery = document.querySelector(".gallery");
+            //Insertion de la balise figure => enfant de gallery
+            gallery.appendChild(figureElement);
+            //Insertion de la balise img + affichage élément => enfant de figure
+            figureElement.appendChild(imgElement);
+            //Insertion de la balise figcation + affichage élément => enfant de figure
+            figureElement.appendChild(figcaptionElement);
+        }
     });
-    const boutonsFiltersObjets = document.querySelector(".cat-objets");
-    boutonsFiltersObjets.addEventListener("click", function() {
-    //Fonction filter par id de la catégorie objets  
-        const categoriesFilter = works.filter(function (work){
-        return work.category.id === 1;
+    // Ajout d'un listner sur le bouton cat-objets
+    const boutonCatObjets = document.querySelector(".cat-objets");
+    boutonCatObjets.addEventListener("click", function(){
+        //Fonction filter par id de la catégorie objets
+        const categoriesFiltered = works.filter(function(works){
+            return works.categoryId === 1;
         });
-    console.log(categoriesFilter);
+        // Effacement de la balise gallery    
+        document.querySelector(".gallery").innerHTML = "";
+        // Affichage des éléments filtrés
+        for (let i = 0 ; i < categoriesFiltered.length ; i++){
+        // Création des éléments dans le DOM
+            // Création de la balise figure
+            const figureElement = document.createElement("figure");
+            // Création de la balise img (enfant de figure)
+            const imgElement = document.createElement("img");
+            // Affichage et identification de l'élément dans la baslise img depuis la source dans la route de l'API
+            imgElement.src = categoriesFiltered[i].imageUrl;
+            // Création de la balise figcation (enfant de figure)
+            const figcaptionElement = document.createElement("figcaption");
+            // Affichage et identification de l'élément dans le texte de la balise figcaption depuis la source dans la route de l'API
+            figcaptionElement.innerText = categoriesFiltered[i].title;
+        // Rattacher éléments créés au parent
+            //Ciblage de l'élémnent parent
+            const gallery = document.querySelector(".gallery");
+            //Insertion de la balise figure => enfant de gallery
+            gallery.appendChild(figureElement);
+            //Insertion de la balise img + affichage élément => enfant de figure
+            figureElement.appendChild(imgElement);
+            //Insertion de la balise figcation + affichage élément => enfant de figure
+            figureElement.appendChild(figcaptionElement);
+        }
     });
-    const boutonsFiltersAppartements = document.querySelector(".cat-appt");
-    boutonsFiltersAppartements.addEventListener("click", function() {
-    //Fonction filter par id de la catégorie appartements  
-        const categoriesFilter = works.filter(function (work){
-        return work.category.id === 2;
+
+    // Ajout d'un listner sur le bouton cat-appt
+    const boutonCatAppt = document.querySelector(".cat-appt");
+    boutonCatAppt.addEventListener("click", function(){
+        //Fonction filter par id de la catégorie appartement
+        const categoriesFiltered = works.filter(function(works){
+            return works.categoryId === 2;
         });
-    console.log(categoriesFilter);
-    });
-    const boutonsFiltersHotelsRestaurants = document.querySelector(".cat-hotels-restaurants");
-    boutonsFiltersHotelsRestaurants.addEventListener("click", function() {
-    //Fonction filter par id de la catégorie hotels et restaurants  
-        const categoriesFilter = works.filter(function (work){
-        return work.category.id === 3;
+        // Effacement de la balise gallery    
+        document.querySelector(".gallery").innerHTML = "";
+        // Affichage des éléments filtrés
+        for (let i = 0 ; i < categoriesFiltered.length ; i++){
+        // Création des éléments dans le DOM
+            // Création de la balise figure
+            const figureElement = document.createElement("figure");
+            // Création de la balise img (enfant de figure)
+            const imgElement = document.createElement("img");
+            // Affichage et identification de l'élément dans la baslise img depuis la source dans la route de l'API
+            imgElement.src = categoriesFiltered[i].imageUrl;
+            // Création de la balise figcation (enfant de figure)
+            const figcaptionElement = document.createElement("figcaption");
+            // Affichage et identification de l'élément dans le texte de la balise figcaption depuis la source dans la route de l'API
+            figcaptionElement.innerText = categoriesFiltered[i].title;
+
+        // Rattacher éléments créés au parent
+            //Ciblage de l'élémnent parent
+            const gallery = document.querySelector(".gallery");
+            //Insertion de la balise figure => enfant de gallery
+            gallery.appendChild(figureElement);
+            //Insertion de la balise img + affichage élément => enfant de figure
+            figureElement.appendChild(imgElement);
+            //Insertion de la balise figcation + affichage élément => enfant de figure
+            figureElement.appendChild(figcaptionElement);
+        }
+    }); 
+
+    // Ajout d'un listner sur le bouton cat-hotels-restaurants
+    const boutonCatHotelsRestaurants = document.querySelector(".cat-hotels-restaurants");
+    boutonCatHotelsRestaurants.addEventListener("click", function(){
+        //Fonction filter par id de la catégorie hotels et restaurants
+        const categoriesFiltered = works.filter(function(works){
+            return works.categoryId === 3;
         });
-    console.log(categoriesFilter);
-    });
+        // Effacement de la balise gallery    
+        document.querySelector(".gallery").innerHTML = "";
+        // Affichage des éléments filtrés
+        for (let i = 0 ; i < categoriesFiltered.length ; i++){
+        // Création des éléments dans le DOM
+            // Création de la balise figure
+            const figureElement = document.createElement("figure");
+            // Création de la balise img (enfant de figure)
+            const imgElement = document.createElement("img");
+            // Affichage et identification de l'élément dans la baslise img depuis la source dans la route de l'API
+            imgElement.src = categoriesFiltered[i].imageUrl;
+            // Création de la balise figcation (enfant de figure)
+            const figcaptionElement = document.createElement("figcaption");
+            // Affichage et identification de l'élément dans le texte de la balise figcaption depuis la source dans la route de l'API
+            figcaptionElement.innerText = categoriesFiltered[i].title;
+
+        // Rattacher éléments créés au parent
+            //Ciblage de l'élémnent parent
+            const gallery = document.querySelector(".gallery");
+            //Insertion de la balise figure => enfant de gallery
+            gallery.appendChild(figureElement);
+            //Insertion de la balise img + affichage élément => enfant de figure
+            figureElement.appendChild(imgElement);
+            //Insertion de la balise figcation + affichage élément => enfant de figure
+            figureElement.appendChild(figcaptionElement);
+        }
+  });
 }
-// Appel de la fonction de récupétation des travaux
-getWorks()
-
-function addWork (work){
-// Création des éléments dans le DOM
-    // Création de la balise figure
-    const figureElement = document.createElement("figure");
-    // Création de la balise img (enfant de figure)
-    const imgElement = document.createElement("img");
-    // Affichage et identification de l'élément dans la baslise img depuis la source dans la route de l'API
-    imgElement.src = work.imageUrl;
-    // Création de la balise figcation (enfant de figure)
-    const figcaptionElement = document.createElement("figcaption");
-    // Affichage et identification de l'élément dans le texte de la balise figcaption depuis la source dans la route de l'API
-    figcaptionElement.innerText = work.title;
-
-// Rattacher éléments créés au parent
-    //Ciblage de l'élémnent parent
-    const gallery = document.querySelector(".gallery");
-    //Insertion de la balise figure => enfant de gallery
-    gallery.appendChild(figureElement);
-    //Insertion de la balise img + affichage élément => enfant de figure
-    figureElement.appendChild(imgElement);
-    //Insertion de la balise figcation + affichage élément => enfant de figure
-    figureElement.appendChild(figcaptionElement);
-}
-
-
-
-
