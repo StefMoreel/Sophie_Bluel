8678e19b9551201613738a5947103034345e5adf
Stephane Moreel <stef.ksp@gmail.com>
2025-06-04 17:30:59 +0200

    Création boutons filltres par catégorie dynamiques à l'appel de l'API route "categories"

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -9,26 +9,26 @@ async function callApiWorks() {
     showWorks(works);
 
 function addWork(work){    
-        // Création des éléments dans le DOM
-        // Création de la balise figure
-        const figureElement = document.createElement("figure");
-        // Création de la balise img (enfant de figure)
-        const imgElement = document.createElement("img");
-        // Affichage et identification de l'élément dans la baslise img depuis la source dans la route de l'API
-        imgElement.src = work.imageUrl;
-        // Création de la balise figcation (enfant de figure)
-        const figcaptionElement = document.createElement("figcaption");
-        // Affichage et identification de l'élément dans le texte de la balise figcaption depuis la source dans la route de l'API
-        figcaptionElement.innerText = work.title;
-    // Rattacher éléments créés au parent
-        //Ciblage de l'élémnent parent
-        const gallery = document.querySelector(".gallery");
-        //Insertion de la balise figure => enfant de gallery
-        gallery.appendChild(figureElement);
-        //Insertion de la balise img + affichage élément => enfant de figure
-        figureElement.appendChild(imgElement);
-        //Insertion de la balise figcation + affichage élément => enfant de figure
-        figureElement.appendChild(figcaptionElement);
+    // Création des éléments dans le DOM
+    // Création de la balise figure
+    const figureElement = document.createElement("figure");
+    // Création de la balise img (enfant de figure)
+    const imgElement = document.createElement("img");
+    // Affichage et identification de l'élément dans la baslise img depuis la source dans la route de l'API
+    imgElement.src = work.imageUrl;
+    // Création de la balise figcation (enfant de figure)
+    const figcaptionElement = document.createElement("figcaption");
+    // Affichage et identification de l'élément dans le texte de la balise figcaption depuis la source dans la route de l'API
+    figcaptionElement.innerText = work.title;
+// Rattacher éléments créés au parent
+    //Ciblage de l'élémnent parent
+    const gallery = document.querySelector(".gallery");
+    //Insertion de la balise figure => enfant de gallery
+    gallery.appendChild(figureElement);
+    //Insertion de la balise img + affichage élément => enfant de figure
+    figureElement.appendChild(imgElement);
+    //Insertion de la balise figcation + affichage élément => enfant de figure
+    figureElement.appendChild(figcaptionElement);
  }
 
 function showWorks(worksList){
@@ -40,39 +40,56 @@ function showWorks(worksList){
     }
 }
 
-    //Ajout d'un listener sur le bouton "Tous"
-    const boutonsFiltersTous = document.querySelector(".all");
-    boutonsFiltersTous.addEventListener("click", function(){
-        showWorks(works);
+async function callApiCategories() {
+    //Requete de la route categories de l'API (GET)
+    const reponse = await fetch("http://localhost:5678/api/categories");
+    //Réponse de la requete au format JSON
+    const repJson = await reponse.json();
+    return repJson;
+ }
+    const categories = await callApiCategories();
+
+
+    //Création de la balise ul
+    const ulElement = document.createElement("ul");
+    //Ciblage de l'élément parent pour ul
+    const filters = document.querySelector(".filters");
+    //Insertion de ul dans filters
+    filters.appendChild(ulElement);
+
+    //Appel de la fonction pour ajouter 1 bouton "Tous" à la balise ul
+    addButtonFilter(ulElement,"Tous", 0);
+
+
+    for (let i = 0; i < categories.length ; i++){
+        const category = categories[i]; 
+    addButtonFilter(ulElement, category.name, category.id);
+    }
+
+function addButtonFilter (parent, text, id){      
+    //Création des éléments pour les boutons dynamiques dans le DOM
+    //Création de la balise li
+    const liElement = document.createElement("li");
+    //création de la balise button
+    const buttonElement = document.createElement("button");
+    //Affichage du texte du bouton
+    buttonElement.innerText = text;
+    buttonElement.setAttribute("name", id)
+    //Ciblage de l'élement ul et insertion de li
+    parent.appendChild(liElement);
+    //Ciblage de l'élement li et insertion de button
+    liElement.appendChild(buttonElement);
+      
+    buttonElement.addEventListener("click", function(event){
+        const buttonName = parseInt(event.currentTarget.attributes.name.value);
+            if(buttonName === 0){
+                showWorks(works);
+            }else{
+                const categoriesFiltered = works.filter(function(work){
+                    return work.categoryId === buttonName;   
+                })
+            showWorks(categoriesFiltered);
+            };
     });
 
-    // Ajout d'un listner sur le bouton cat-objets
-    const boutonCatObjets = document.querySelector(".cat-objets");
-    boutonCatObjets.addEventListener("click", function(){
-        //Fonction filter par id de la catégorie objets
-        const categoriesFiltered = works.filter(function(work){
-             return work.categoryId === 1;
-         });
-        showWorks(categoriesFiltered);
-    });
-
-    // Ajout d'un listner sur le bouton cat-appt
-    const boutonCatAppt = document.querySelector(".cat-appt");
-    boutonCatAppt.addEventListener("click", function(){
-        //Fonction filter par id de la catégorie appartement
-        const categoriesFiltered = works.filter(function(works){
-            return works.categoryId === 2;
-        });
-        showWorks(categoriesFiltered);
-    }); 
-
-    // Ajout d'un listner sur le bouton cat-hotels-restaurants
-    const boutonCatHotelsRestaurants = document.querySelector(".cat-hotels-restaurants");
-    boutonCatHotelsRestaurants.addEventListener("click", function(){
-        //Fonction filter par id de la catégorie hotels et restaurants
-        const categoriesFiltered = works.filter(function(works){
-            return works.categoryId === 3;
-        });
-        showWorks(categoriesFiltered);
-    });
-
+ }
